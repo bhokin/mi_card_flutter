c5a359e9d5608eabefd7e6664a4de8fa639ad195
bhokin <zbeamz@hotmail.com>
2022-12-28 11:43:35 +0700

    SafeArea widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            // margin: EdgeInsets.all(20), // 4 side
+            // margin: EdgeInsets.fromLTRB(left, top, right, bottom),
+            // margin: EdgeInsets.only(left: 20, top: 60),
+            margin: EdgeInsets.symmetric(
+              vertical: 20,
+              horizontal: 10,
+            ),
+            padding: EdgeInsets.all(20),
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
     );
   }
